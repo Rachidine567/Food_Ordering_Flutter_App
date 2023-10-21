74b8f7dfcc634c09696a9a8d07a692db06908d2c
Rachidine567 <rmahamoud525@rku.ac.in>
2023-10-21 00:39:14 -0700

    First Commit

---
+++ lib/loginpart/wecome_page/homepage/data.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Data {
+  static List foods = [
+    "pilaf1",
+    "dishes",
+    "salad2",
+    "salad",
+  ];
+
+  static List foods2 = [
+    "dishes",
+    "salad",
+  ];
+
+  static List<Color> bgColors = [
+    Color(0xFFFBDCDA),
+    Color(0xFFD4EEF3),
+    Color(0xFFFAE6D5),
+    Color(0xFFEFCFE7),
+  ];
+}
